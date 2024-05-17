f11e1d8e09c50523058336ce0907de82f3e86a71
boywonder3 <opeyemiisaacboywonder@gmail.com>
2024-05-17 11:43:30 +0100

    code refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Courses from "./section/Courses";
 import Home from "./section/Home";
 
 export default function App() {
   return (
-    <>
+    <div className="">
       <Home />
-    </>
+      <Courses />
+    </div>
   );
 }
